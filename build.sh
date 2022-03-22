32e5484108e3f1d020685bd1ca6dc59b0c90f0ac
Evan Ovadia <verdagon@gmail.com>
2022-03-22 15:19:04 -0400

    Cleaned up RAII++ to be clearer to readers

--- build.sh
+++ build.sh
@@ -7,12 +7,4 @@ if [ "$VALEC_DIR" == "" ]; then
 fi
 shift;
 
-STDLIB_DIR="$1"
-if [ "$STDLIB_DIR" == "" ]; then
-  echo "Please supply the stdlib directory."
-  echo "Example: ~/stdlib"
-  exit
-fi
-shift;
-
-$VALEC_DIR/valec build stdlib=$STDLIB_DIR/src atharia=src domino=~/VDomino/src terrain=~/Terrain/src rocketvale=~/RocketVale/src rocketvale=~/RocketVale/src/native/rust/target/debug/librocketvale.a valejson=~/ValeJSON/src parseiter=~/ParseIter/src --output_dir build --region_override resilient-v3 --llvm_ir true
+$VALEC_DIR/valec build atharia=src domino=~/VDomino/src terrain=~/Terrain/src rocketvale=~/RocketVale/src rocketvale=~/RocketVale/src/native/rust/target/debug/librocketvale.a valejson=~/ValeJSON/src parseiter=~/ParseIter/src --output_dir build --region_override resilient-v3 --llvm_ir true
